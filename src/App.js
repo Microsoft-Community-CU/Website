82939a53631903990af0502961aa4d5dfd8609b4
Souvik <souvikde.ns@gmail.com>
2020-08-08 23:06:18 +0530

    created multi page support 😊

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+// importing pages 
+import Index from './pages/index'
 
 const App = props => {
   return (
     <div>
+
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Index} />
+        </Switch>
+      </Router>
 
     </div>
   );
