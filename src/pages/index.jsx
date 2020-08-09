dd84fc89edda1a5e6a6225b9a32f44b9bd86cf8b
Souvik <souvikde.ns@gmail.com>
2020-08-09 15:03:23 +0530

    Designing 🔥

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 
+import {
+    Container,
+    makeStyles,
+    Typography,
+    Grid
+} from '@material-ui/core'
+
+// importing images 
+import Logo from '../images/logo.png'
+
+const useStyle = makeStyles(theme => ({
+    root: {
+        marginTop: theme.spacing(32)
+    }
+}))
+
 
 const Index = props => {
+    const classes = useStyle()
 
     return (
-        <div>
-            <h1>Index Page</h1>
+        <div className={classes.root}>
+            <Container>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <center>
+                            <img src={Logo} alt="logo" width="60%" />
+                        </center>
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     )
 }
